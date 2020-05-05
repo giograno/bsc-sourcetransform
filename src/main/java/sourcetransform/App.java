023288bad14db9c3dae374a53e7eb8be0ccb3956
Christian Birchler <christian.birchler2@uzh.ch>
2020-05-05 20:56:59 +0200

    Simplify main function
    The main function takes ideally one argument which defines the project folder.

--- src/main/java/sourcetransform/App.java
+++ src/main/java/sourcetransform/App.java
@@ -4,49 +4,27 @@ package sourcetransform;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Optional;
 
-public class App
-{
+public class App {
     // args: path to a maven project
     public static void main(String[] args) throws IOException {
 
+        File mvnProjDir = null;
+
         //System.out.println(args[0]);
-
-        //File mvnProjDir = new File("/home/christian/Desktop/test/");
-        File mvnProjDir = new File("/home/christian/Desktop/hadoop/");
-
-
-        /*
-        - if test folder then modify all test classes (TODO: create function)
-        -
-         */
+        if (args.length == 1) {
+            mvnProjDir = new File(args[0]);
+            if (!mvnProjDir.isDirectory()) {
+                System.out.println("ERROR: Input is not a directory ...");
+                return;
+            }
+        }
+        if (args.length != 1) {
+            System.out.println("WARNING: Use default project directory ...");
+            mvnProjDir = new File("/home/christian/Desktop/my-app");
+        }
 
         TestDirectoryVisitor testDirVisitor = new TestDirectoryVisitor();
         Files.walkFileTree(mvnProjDir.toPath(), testDirVisitor);
-
-
-
-
-
-
-
-        //String path = "/home/christian/Desktop/bsc-sourcetransform/src/main/resources/TestFile.java";
-        String path = "/home/christian/Desktop/bsc-sourcetransform/src/main/resources/TestKerberosAuthenticator.java";
-
-        File file = new File(path);
-        PerformanceMetricsInjector pmi = new PerformanceMetricsInjector(file);
-        //pmi.injectMeasurementCode();
-        //pmi.writeToConsole();
-        //pmi.writeToFile();
-
-        // add dependency to pom.xml
-
-        // traverse over all test classes
-
-            // add injection code to the test cases (methods with Test annotation)
-
     }
 }
