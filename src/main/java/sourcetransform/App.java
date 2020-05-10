1d5518cb20df86ef5c49197966da446f7478a087
Christian Birchler <christian.birchler2@uzh.ch>
2020-05-10 19:16:29 +0200

    pom.xml parsing

--- src/main/java/sourcetransform/App.java
+++ src/main/java/sourcetransform/App.java
@@ -1,13 +1,20 @@
 package sourcetransform;
 
 
-import java.io.File;
-import java.io.IOException;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+import org.apache.maven.model.io.xpp3.MavenXpp3Writer;
+import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
+
+import java.io.*;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
     // args: path to a maven project
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
         File mvnProjDir = null;
 
@@ -21,10 +28,61 @@ public class App {
         }
         if (args.length != 1) {
             System.out.println("WARNING: Use default project directory ...");
-            mvnProjDir = new File("/home/christian/Desktop/my-app");
+            mvnProjDir = new File("/home/christian/Desktop/hadoop");
         }
 
+        // Travers the directory tree with a visitor to modify the test code
         TestDirectoryVisitor testDirVisitor = new TestDirectoryVisitor();
-        Files.walkFileTree(mvnProjDir.toPath(), testDirVisitor);
+        try {
+            Files.walkFileTree(mvnProjDir.toPath(), testDirVisitor);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Add metric dependencies to the pom
+        try {
+            addMetricDepsToPom(new File(mvnProjDir.getAbsolutePath()+"/pom.xml"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private static void addMetricDepsToPom(File pomFile) throws IOException, XmlPullParserException {
+        System.out.println("Add dependencies to pom: ");
+        System.out.println(pomFile.getAbsolutePath());
+
+        // define metrics dependencies
+        Dependency metricsCore = new Dependency();
+        metricsCore.setGroupId("io.dropwizard.metrics");
+        metricsCore.setArtifactId("metrics-core");
+        metricsCore.setVersion("3.2.6");
+        //metricsCore.setVersion("3.2.2");
+
+        Dependency metricsJvm = new Dependency();
+        metricsJvm.setGroupId("io.dropwizard.metrics");
+        metricsJvm.setArtifactId("metrics-jvm");
+        metricsJvm.setVersion("3.2.6");
+        //metricsJvm.setVersion("3.2.2");
+
+        // setup maven pom model
+        Reader reader = new FileReader(pomFile);
+        MavenXpp3Reader xpp3Reader = new MavenXpp3Reader();
+        Model model = xpp3Reader.read(reader);
+
+        List<Dependency> dependencyList = new ArrayList<Dependency>(model.getDependencies());
+        dependencyList.add(metricsCore);
+        dependencyList.add(metricsJvm);
+
+
+        model.setDependencies(dependencyList);
+
+        MavenXpp3Writer xpp3Writer = new MavenXpp3Writer();
+
+        xpp3Writer.write(new FileWriter(pomFile), model);
+
+
+        reader.close();
     }
 }
